6f67ebbc8040fb1b688312080c80d7bc0f115575
Leonardo Marinho <leowalkg6@gmail.com>
2023-09-01 16:58:09 -0300

    construindo class pj

--- HenrancaPoli/HenrancaPoli/Entities/PessoaJuridica.cs
+++ HenrancaPoli/HenrancaPoli/Entities/PessoaJuridica.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace HenrancaPoli.Entities
 {
-    internal class PessoaJuridica
+    public class PessoaJuridica : Contribuintes
     {
+        public int NumberOfEmployees { get; set; }
+
+        public PessoaJuridica() { }
+
+        public PessoaJuridica(string name,double anualIncome , int numberOfEmpl) 
+            : base(name,anualIncome) {
+        
+        }
+
+        public override double Tax()
+        {
+            
+        }
+
+
+
     }
 }
