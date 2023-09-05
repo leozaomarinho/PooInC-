c5ee312a58b62960e74de13fbad73f017252467c
Leonardo Marinho <leowalkg6@gmail.com>
2023-09-05 20:29:11 -0300

    tentando ajustar exceção

--- PagamantoFunc/Program.cs
+++ PagamantoFunc/Program.cs
@@ -61,6 +61,9 @@ namespace Pagamento
                 catch(SystemException)
                 {
                     Console.WriteLine("Entrada invalida, Digite uma entrada valida!");
+                    Console.WriteLine($"Employe number #{i} data:");
+                    Console.Write("OutSourced (y/n)? ");
+                    char ch = char.Parse(Console.ReadLine());
                 }
                 Console.WriteLine("--------------------------------------------------");
             }
